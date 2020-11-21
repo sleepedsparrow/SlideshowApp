96c1d6770182d946c5a5d3e160a23f0259aaf650
momoka.kato <mezzo52841@gmail.com>
2020-11-21 12:42:00 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 加藤桃香 on 2020/11/21.
+//  Copyright © 2020 momoka.kato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
